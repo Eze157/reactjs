580b9a7e76b6b5a25436af4ab414ba1030ba91ce
Ezequiel Castillo <eze15794@hotmail.com>
2024-02-12 16:25:52 -0300

    agregue imagenes

--- proyectoeze/src/components/Item/Item.jsx
+++ proyectoeze/src/components/Item/Item.jsx
@@ -4,7 +4,7 @@ const Item = ({product}) => {
     return (
         <div key={product.id}>
         <h2>{product.name}</h2>
-        <h2>{product.img}</h2>
+        <img src={product.img} alt={product.nombre} />
         <h2>{product.price}</h2>
         <h2>{product.stock}</h2>
         </div>
